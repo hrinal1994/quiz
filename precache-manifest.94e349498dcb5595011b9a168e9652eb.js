841c466f77f6ded2c1de1bce4e28cd33260872f1
hrinal <hrinal.bunkar@zf.com>
2020-10-07 16:47:39 +0530

    Updates

---
+++ precache-manifest.94e349498dcb5595011b9a168e9652eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4e4b702346b8ac060063b2afb0ea44",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "7502dd5a42a3325067dd",
+    "url": "/quiz/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "35702df8c95caf6ea019",
+    "url": "/quiz/static/css/main.176928ee.chunk.css"
+  },
+  {
+    "revision": "7502dd5a42a3325067dd",
+    "url": "/quiz/static/js/2.a8d7cade.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quiz/static/js/2.a8d7cade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35702df8c95caf6ea019",
+    "url": "/quiz/static/js/main.db58b5c0.chunk.js"
+  },
+  {
+    "revision": "1e391d41ddb7f733dbeb",
+    "url": "/quiz/static/js/runtime-main.0f5f6437.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1049b9501fbb6bc2a2dabb9f58244f41",
+    "url": "/quiz/static/media/texture10.1049b950.jpg"
+  }
+]);
